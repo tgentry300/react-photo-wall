8e3ebddc88c98cfdaee46ea778a465f64527d89d
Taylor Gentry <taylor.gentry300@gmail.com>
2018-08-06 01:17:14 -0400

    somewhat finished, trying to figure how to properly use this api

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 // This URL can be combined with an photo id to fetch an photo.
 const PHOTO_URL = "https://picsum.photos/200?photo=";
@@ -7,17 +7,56 @@ const PHOTO_URL = "https://picsum.photos/200?photo=";
 // about various photos.
 const PHOTO_LIST_URL = "https://picsum.photos/list";
 
+
 class App extends Component {
   // 1. Declare a state object that will be used to track an array of photos
-  state = {
-  }
+  state = {};
 
   // 2. Declare a life cycle method
   // This life cycle method should:
   //  - will be called after the component is initially rendered
   // - will fetch an array of photos
   // - will add that array of photos to state once received
+
+  
+  
+
+  componentWillMount() {
+    const pictures = []
+    const photoIdNumbers = []
+    const filenames = []
+    fetch(PHOTO_LIST_URL)
+    .then(res => {
+      return res.json()
+    })
+    .then(data => {
+      // console.log
+      data.forEach(async photo => {
+        await filenames.push(photo.filename)
+
+        if (!photoIdNumbers.includes(photo.id)){
+          photoIdNumbers.push(photo.id)
+        }
+
+        fetch(PHOTO_URL + photo.id)
+        .then(res =>{
+          return res
+        })
+        .then( data => {
+          // console.log(data)
+
+          pictures.push(data)
+          // console.log(pictures)
+          this.setState({photos: pictures, ids: photoIdNumbers, filenames: filenames })
+        })
+      })
+    })
+    // console.log(this.state)
+  }
+
   render() {
+    // const ids = this.state.ids
+
     const { photos = [] } = this.state;
     return (
       <React.Fragment>
@@ -25,20 +64,17 @@ class App extends Component {
           <h1>Photo Wall</h1>
           <p>
             Start by reading App.jsx and completing the numbered steps.
-            Afterward, delete this paragraph. Then, open up App.css and
-            complete the instructions there.
+            Afterward, delete this paragraph. Then, open up App.css and complete
+            the instructions there.
           </p>
         </header>
         <div className="collage">
-            {/* We use map here because Array.prototype.map is an expression,
+          {/* We use map here because Array.prototype.map is an expression,
               * and for loops are not. You'll learn more about this soon! 
               */}
-            {photos.map( photo => 
-                <img alt={/* 3. Fill me in with the photo's filename */ ""}
-                     key={/* 4. Fill me in with the photo's id */ ""}
-                     src={/* 5. Fill me in with the photo's URL */ ""}
-                />
-            )}
+          {photos.map(photo => 
+            <img alt={photo.url} key={photo.url} src={photo.url} />
+          )}
         </div>
       </React.Fragment>
     );
